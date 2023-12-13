06ae55587b45dc9a318c53f4386aa521bc21266a
Evaldo-Gomes <evaldo_gomes@hotmail.com>
2023-12-13 14:08:20 -0300

    Implemento acelerador

--- testeVeiculo.py
+++ testeVeiculo.py
@@ -2,8 +2,8 @@
 
 from veiculo import Veiculo
 
+# Criar um objeto com suas características ("Como construir o objeto)
 minhaCaranga = Veiculo('Fiat', '147', 'Amarelo', 0)
-
 # Exibindo a minha caranga
 print('\n\t\t\t -- Minha Caranga  --')
 print(minhaCaranga)
@@ -11,7 +11,14 @@ print(minhaCaranga)
 # Acelerando a minha caranga
 for i in range(0, 200):
     minhaCaranga.acelerar()
-
 # Exibindo a minha caranga acelerada
 print('\n\t\t\t -- Minha Caranga Acelerada  --')
+print(minhaCaranga)
+
+# Freando a minha caranga
+for i in range(0, 200):
+    minhaCaranga.frear()
+
+# Exibindo a minha caranga freada
+print('\n\t\t\t -- Minha Caranga Freada --')
 print(minhaCaranga)
--- veiculo.py
+++ veiculo.py
@@ -11,29 +11,25 @@ class Veiculo(object):
     # Encapsulamento
     def setMarca(self, marca):
         self.marca = marca
-
     def getMarca(self):
         return self.marca
 
     def setModelo(self, modelo):
         return self.modelo
-
     def getModelo(self):
         return self.modelo
 
     def setCor(self, cor):
         self.cor = cor
-
     def getCor(self):
         return self.cor
 
     def setVelocidade(self, velocidade):
         return self.velocidade
-
     def getVelocidade(self):
         return self.velocidade
 
-    # Retorno de dados da classe: Transforma os dados da classe em uma cadeia de string
+    # Retorno de dados da classe - Método String: Transforma os dados da classe em uma cadeia de string
     def __str__(self):
         return (
             '\n Marca: ' + str(self.getMarca()) +
@@ -45,3 +41,8 @@ class Veiculo(object):
     def acelerar(self):
         if self.velocidade < 120:
             self.velocidade += 1
+
+    # Método frear
+    def frear(self):
+        if self.velocidade > 0:
+            self.velocidade -= 1
